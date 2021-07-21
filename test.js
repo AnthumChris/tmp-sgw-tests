ac330c3b0799835f6f64b70caa0d6f507c1700e2
Chris McGowan <chrsmcgw@amazon.com>
2021-07-21 12:51:18 -0600

    use name simulateParallelTest

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 const timeoutTest = 5_000
 
-function simulateDelayedTest(done) {
+function simulateParallelTest(done) {
   setTimeout(() => done(), timeoutTest)
 }
 
 describe('My Tests', function() {
   this.timeout(timeoutTest + 500) // ensure tests don't timeout
 
-  it('should create gateway 1: File', simulateDelayedTest)
-  it('should create gateway 2: Tape', simulateDelayedTest)
-  it('should create gateway 3: Foo',  simulateDelayedTest)
-  it('should create gateway 4: Bar',  simulateDelayedTest)
-  it('should create gateway 5: Baz',  simulateDelayedTest)
-  it('should create gateway 6: Zee',  simulateDelayedTest)
+  it('should create gateway 1: File', simulateParallelTest)
+  it('should create gateway 2: Tape', simulateParallelTest)
+  it('should create gateway 3: Foo',  simulateParallelTest)
+  it('should create gateway 4: Bar',  simulateParallelTest)
+  it('should create gateway 5: Baz',  simulateParallelTest)
+  it('should create gateway 6: Zee',  simulateParallelTest)
 })
